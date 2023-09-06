2eff8f74b0227b2fa13301302d57cbe7c454f6b0
anthopy <anthonymenghi@protonmail.com>
2023-09-06 11:30:00 +0200

    update: variables in papiercaillouxciseaux_obscur

--- Atelier/0/papiercaillouxciseaux_obscur.py
+++ Atelier/0/papiercaillouxciseaux_obscur.py
@@ -1,147 +1,150 @@
 import random
 
-cpo = input("Voulez-vous jouer contre l'ordinateur (Max 5 parties) O/N ? " )
-if cpo != 'O' :
-    if cpo != 'N' :
-        print("Je n'ai pas compris votre réponse")
-if cpo == 'O':
-    n1 = input("Quel est votre nom ? ")
-    print("Bienvenu ",n1, " nous allons jouer ensemble \n")
-    n2 = 'Machine'
-s1 = 0
-np = 0
-if cpo == 'N':
-    n1 = input("Quel est votre nom ? ")
-    print("Bienvenu ",n1, " nous allons jouer ensemble")
-    n2 = input("Quel est le nom du deuxième joueur ?")
-    print("Bienvenu ",n2, " nous allons jouer ensemble \n")
+# init variables
+input_init = input("Voulez-vous jouer contre l'ordinateur (Max 5 parties) O/N ? " ).upper()
 
-c = True
-p2 = 0
-while c == True:
-    np += 1 
-    c1 = input("{nom} faîtes votre choix parmi (pierre, papier, ciseaux): ".format(nom=n1))
-    if c1 != 'pierre':
-        if c1 != 'papier':
-            if c1 != 'ciseaux':
-                c1ok = False
+score_player1= 0
+score_player2 = 0
+
+round_count= 0
+gameActive = False
+
+# init players
+if input_init != 'O' and input_init != 'N' :
+    print("Je n'ai pas compris votre réponse")
+    
+else : 
+    player = input("Quel est votre nom ? ")
+    gameActive= True
+    if input_init == 'O':
+        print("Bienvenu ",player, " nous allons jouer ensemble \n")
+        player2 = 'Machine'
+    else :
+        print("Bienvenu ",player, " nous allons jouer ensemble")
+        player2 = input("Quel est le nom du deuxième joueur ?")
+        print("Bienvenu ",player2, " nous allons jouer ensemble \n")
+        
+
+
+while gameActive== True:
+    
+    round_count+= 1 
+    
+    player_choice = input(f"{player} faîtes votre choix parmi (pierre, papier, ciseaux): ").lower()
+    
+    if player_choice != 'pierre' and player_choice != 'papier' and player_choice != 'ciseaux' :
+                player_choiceok = False
                 print("Je n'ai pas compris votre réponse")
-                while c1ok == False :
-                    print("Joueur ", n1)
-                    c1 = input(" faîtes votre choix parmi (pierre, papier, ciseaux): ")
-                    c1ok = True
-                    if c1 != 'pierre': 
-                        if c1 != 'papier':
-                            if c1 != 'ciseaux':
-                                c1ok = False
+                
+                while player_choiceok == False :
+                    print("Joueur ", player)
+                    player_choice = input(" faîtes votre choix parmi (pierre, papier, ciseaux): ").lower()
+                    player_choiceok = True
+                    if player_choice != 'pierre' and player_choice != 'papier' and player_choice != 'ciseaux': 
+                        player_choiceok = False
 
-    if n2 == 'Machine': 
-        #Ici il faudrait plutôt vérifier que cpo == 'O' autrement
-        #il y a un problème si le joueur 2 s'appelle Machine !
-        e2 = ['papier','pierre','ciseaux'][random.randint(0, 2)]
+    if input_init == 'O': 
+       machine_choice= ['papier','pierre','ciseaux'][random.randint(0, 2)]
 
-
-    if n2 != 'Machine':
-        print("Joueur", n2)
-        e2 = input("faîtes votre choix parmi (pierre, papier, ciseaux): ")
-        if e2 != 'pierre':
-            if e2 != 'papier':
-                if e2 != 'ciseaux':
+    if input_init == 'N':
+        print("Joueur", player2)
+        player2_choice= input("faîtes votre choix parmi (pierre, papier, ciseaux): ").lower()
+        if machine_choice!= 'pierre':
+            if machine_choice!= 'papier':
+                if machine_choice!= 'ciseaux':
                     j2bon = False
                     print("Je n'ai pas compris votre réponse")
                     while not j2bon :
-                        print("Joueur ", n2)
-                        e2 = input(" faîtes votre choix parmi (pierre, papier, ciseaux): ")
+                        print("Joueur ", player2)
+                        machine_choice= input(" faîtes votre choix parmi (pierre, papier, ciseaux): ")
                         j2bon = True
-                        if e2 != 'pierre': 
-                            if e2 != 'papier':
-                                if e2 != 'ciseaux':
+                        if machine_choice!= 'pierre': 
+                            if machine_choice!= 'papier':
+                                if machine_choice!= 'ciseaux':
                                     j2bon = False
 
     #On affiche les choix de chacun
-    print("Si on récapitule :",n1, c1, "et", n2, e2,"\n")
+    print("Si on récapitule :",player, player_choice, "et", player2, machine_choice,"\n")
 
 
     #On regarde qui a gagné cette manche on calcule les points et on affiche le résultat
-    if c1 == 'papier' and e2 == 'papier' :
-        w12 = "aucun de vous, vous être ex æquo"
-        s1 = s1 + 0
-        p2 = p2 + 0
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'papier' and machine_choice== 'papier' :
+        winner = "aucun de vous, vous être exequo"
+        score_player1= score_player1+ 0
+        score_player2 = score_player2 + 0
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if c1 == 'pierre' and e2 == 'papier' :
-        w12 = n2
-        s1 = s1 + 0
-        p2 = p2 + 1
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'pierre' and machine_choice== 'papier' :
+        winner = player2
+        score_player1= score_player1+ 0
+        score_player2 = score_player2 + 1
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if c1 == 'pierre' and e2 == 'pierre' :
-        w12 = "aucun de vous, vous être ex æquo"
-        s1 = s1 + 0
-        p2 = p2 + 0
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'pierre' and machine_choice== 'pierre' :
+        winner = "aucun de vous, vous être ex æquo"
+        score_player1= score_player1+ 0
+        score_player2 = score_player2 + 0
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if c1 == 'pierre' and e2 == 'ciseaux' :
-        w12 = n1
-        s1 = s1 + 1
-        p2 = p2 + 0
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'pierre' and machine_choice== 'ciseaux' :
+        winner = player
+        score_player1= score_player1+ 1
+        score_player2 = score_player2 + 0
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if c1 == 'papier' and e2 == 'ciseaux' :
-        w12 = n2
-        s1 = s1 + 0
-        p2 = p2 + 1
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'papier' and machine_choice== 'ciseaux' :
+        winner = player2
+        score_player1= score_player1+ 0
+        score_player2 = score_player2 + 1
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if c1 == 'papier' and e2 == 'pierre' :
-        w12 = n1
-        s1 = s1 + 1
-        p2 = p2 + 0
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'papier' and machine_choice== 'pierre' :
+        winner = player
+        score_player1= score_player1+ 1
+        score_player2 = score_player2 + 0
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if c1 == 'ciseaux' and e2 == 'pierre' :
-        w12 = n2
-        s1 = s1 + 0
-        p2 = p2 + 1
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'ciseaux' and machine_choice== 'pierre' :
+        winner = player2
+        score_player1= score_player1+ 0
+        score_player2 = score_player2 + 1
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if c1 == 'ciseaux' and e2 == 'ciseaux' :
-        w12 = "aucun de vous, vous être ex æquo"
-        s1 = s1 + 0
-        p2 = p2 + 0
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'ciseaux' and machine_choice== 'ciseaux' :
+        winner = "aucun de vous, vous être ex æquo"
+        score_player1= score_player1+ 0
+        score_player2 = score_player2 + 0
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if c1 == 'ciseaux' and e2 == 'papier' :
-        w12 = n1
-        s1 = s1 + 1
-        p2 = p2 + 0
-        print("le gagnant est",w12)
-        print("Les scores à l'issue de cette manche sont donc",n1, s1, "et", n2, p2, "\n")
+    if player_choice == 'ciseaux' and machine_choice== 'papier' :
+        winner = player
+        score_player1= score_player1+ 1
+        score_player2 = score_player2 + 0
+        print("le gagnant est",winner)
+        print("Les scores à l'issue de cette manche sont donc",player, score_player1, "et", player2, score_player2, "\n")
 
-    if np ==1 or np ==2 or np==3 or np==4:
-        c = True
-    if np ==5:
-        c = False
+    if round_count==1 or round_count==2 or round_count==3 or round_count==4:
+        gameActive= True
+    if round_count==5:
+        gameActive= False
         
-    if np ==1 or np ==2 or np==3 or np==4:
-        #On propose de c ou de s'arrêter 
-        go = input("Souhaitez vous refaire une partie {} contre {} ? (O/N) ".format(n1,n2))
+    if round_count==1 or round_count==2 or round_count==3 or round_count==4:
+        #On propose de gameActiveou de s'arrêter 
+        go = input("Souhaitez vous refaire une partie {} contre {} ? (O/N) ".format(player,player2))
         if go == 'O':
-            c = True
+            gameActive= True
         if go == 'N':
-            c = False
+            gameActive= False
         if go != 'O' and go != 'N':
-            c = True
+            gameActive= True
             print("Vous ne répondez pas à la question, on continue ")
-  
         
-if c == False :
-    print("Merci d'avoir joué ! A bientôt")
+print("Merci d'avoir joué ! A bientôt")

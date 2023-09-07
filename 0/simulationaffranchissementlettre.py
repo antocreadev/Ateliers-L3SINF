e7b051cb879ae83429e1d2e6db9353da675e55ee
anthopy <anthonymenghi@protonmail.com>
2023-09-07 09:26:48 +0200

    add: condition before loop - loop for verifie input_type

--- 0/simulationaffranchissementlettre.py
+++ 0/simulationaffranchissementlettre.py
@@ -1,7 +1,4 @@
-
-
 # CONST
-
 # structure : TYPES_LETTERS = { type : [(poids, prix), (poids, prix), ...], type : [(poids, prix), (poids, prix), ...], ...}
 TYPES_LETTERS = {
     "VERTE": [(20, 1.16), (100, 2.32), (250, 4), (500, 6), (1000, 7.5), (3000, 10.5)],
@@ -10,30 +7,32 @@ TYPES_LETTERS = {
 }
 
 # FUNCTIONS
-def affranchissement_lettre(type : str , poid : int) -> float : 
+def affranchissement_lettre(type : str , poids : int) -> float : 
     """
     Calcule le prix d'affranchissement(float) d'une lettre en fonction de son type(int) et de son poids(int)
     type : str
     poids : int
     """
-    type = type.upper()
     # example : prix = TYPES_LETTERS[type][0][1]
+    
+    # if poid exceed the max weight for this type of letter
+    if poids > TYPES_LETTERS[type][-1][0] :
+        return f"Vous dépasser le poid maximum qui est {TYPES_LETTERS[type][-1][0]}g pour ce type de lettre"
+ 
     for i in range(len(TYPES_LETTERS[type])):
-        # print(TYPES_LETTERS[type][i])
-        
-        # if poid exceed the max weight for this type of letter
-        if poid > TYPES_LETTERS[type][-1][0] :
-            return f"Vous dépasser le poid maximum qui est {TYPES_LETTERS[type][-1][0]}g pour ce type de lettre"
-        
         # if poid is in the range of the current weight
-        if poid <= TYPES_LETTERS[type][i][0]:
+        if poids <= TYPES_LETTERS[type][i][0]:
             return f"{TYPES_LETTERS[type][i][1]}€"
 
 
 # EXECUTION
-input_type = input("Quel est le type de votre lettre ? (verte, prioritaire, ecopli) : ").lower().strip()
-input_poinds = int(input("Quel est le poids de votre lettre ? : "))
-
-print(affranchissement_lettre(input_type, input_poinds))
+input_type = input("Quel est le type de votre lettre ? (verte, prioritaire, ecopli) : ").upper().strip()
+# loop to check if input_poids is correct
+while input_type not in TYPES_LETTERS:
+    print("Vous n'avez pas choisi un type de lettre, on continue ")
+    input_type = input("Quel est le type de votre lettre ? (verte, prioritaire, ecopli) : ").upper().strip()
+input_poids = int(input("Quel est le poids de votre lettre ? : "))
+    
+print(affranchissement_lettre(input_type, input_poids))
     
     

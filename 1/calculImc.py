5f08c3e1244deb2e2e6bc3af7ce80d9dff030077
anthopy <anthonymenghi@protonmail.com>
2023-09-07 11:14:37 +0200

    add: error handling and test

--- 1/calculImc.py
+++ 1/calculImc.py
@@ -1,16 +1,18 @@
-# CONST 
+# --- IMPORT ---
+from random import uniform
 
-# IMC Interprétation 
-# < 16,5 dénutrition ou famine -> IF
-
-# 16,5 à 18,5 maigreur
-# 18,5 à 25 corpulence normale 
-# 25 à 30 surpoids 
-# 30 à 35 obésité modérée 
-# 35 à 40 obésité sévère 
-
-# plus de 40 obésité morbide  -> IF
-
+# --- UTILS --- :
+    # On peut parcourir un dictionnaire avec les méthodes suivantes (on ne peut pas parcourir un dictionnaire avec une boucle for classique car un dict n'est pas une sséquence et n'est pas indexé)
+    # keys() : parcourt les clés.
+    # values() : parcourt les valeurs.
+    # items() : parcourt les couples clé-valeur
+    
+    # uniform fonctionne comme randint mais avec des float
+    
+    # (min, max) -> destructuring dans la boucle sont des variables temporaires à la boucle
+    
+    
+# --- CONST ---
 # structure : { "interprétation" : (min, max), ..."}
 IMC = {
     "maigreur" : (16.5, 18.5),
@@ -19,23 +21,40 @@ IMC = {
     "obésité modérée" : (30, 35),
     "obésité sévère" : (35, 40),
 }
-    
-    
 
-# Functions
+# --- Functions ---
 def message_imc(imc : float) -> str :
-    """_summary_
+    """ function that returns a message according to the imc
 
     Args:
-        imc (float): _description_
+        imc (float)
 
     Returns:
-        str: _description_
+        str: iterpretation of the imc
     """
-    for interpretation, (min, max) in IMC.items() :
-        if imc >= min and imc < max :
-            return interpretation
+    message = None
+    if imc < IMC["maigreur"][0] :
+        message = "dénutrition ou famine"
+    elif imc > IMC["obésité sévère"][1] :
+        message = "obésité morbide"
+    else :
+        for interpretation, (min, max) in IMC.items() : # (min, max) -> destructuring
+            if imc >= min and imc < max :
+                message = interpretation
+    return message
 
+def test_message_imc(x : int ) -> str:
+    """ function that tests the function message_imc
+    
+        Args:
+            x (int) : number of tests
 
+        Returns:
+            str: iterpretation of the imc
+    """
+    for k in range(x) :
+        imc = round(uniform(0, x),2)
+        print(f"imc = {imc} : {message_imc(imc)}")
 
-print(message_imc(17))
+# --- MAIN ---
+test_message_imc(50)
